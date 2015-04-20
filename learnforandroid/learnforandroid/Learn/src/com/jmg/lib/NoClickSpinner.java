3700127753a331f04807aa5c26f06e95d855e34a
jhmgbl <jhmgbl@gmail.com>
2015-04-20 16:14:42 +0200

    NoClickSpinner

---
+++ learnforandroid/learnforandroid/Learn/src/com/jmg/lib/NoClickSpinner.java
@@ -0,0 +1,41 @@
+package com.jmg.lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Spinner;
+
+public class NoClickSpinner extends Spinner {
+
+	public NoClickSpinner(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClickSpinner(Context context, int mode) {
+		super(context, mode);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClickSpinner(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClickSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClickSpinner(Context context, AttributeSet attrs,
+			int defStyleAttr, int mode) {
+		super(context, attrs, defStyleAttr, mode);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClickSpinner(Context context, AttributeSet attrs,
+			int defStyleAttr, int defStyleRes, int mode) {
+		super(context, attrs, defStyleAttr, defStyleRes, mode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
