581463940a89df2d610c2614e199d42501cc299c
jhmgbl <jhmgbl@gmail.com>
2015-04-24 17:31:01 +0200

    reset vocabulary after Settings

--- learnforandroid/learnforandroid/Learn/src/com/jmg/learn/MainActivity.java
+++ learnforandroid/learnforandroid/Learn/src/com/jmg/learn/MainActivity.java
@@ -1010,6 +1010,7 @@ public class MainActivity extends ActionBarActivity {
 
 				editor.commit();
 				setTextColors();
+				getVokabel(false, false);
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
